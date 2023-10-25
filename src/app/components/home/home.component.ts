dc0ec34a1adebb22e774a5bd7f3c9af4c39da193
1taimoorsajjad <taimoorsajjad980@gmail.com>
2023-10-26 01:51:41 +0500

    Made changes in the home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
 
+  }
+  downloadCV() {
+
+    const cvUrl = '/assets/TaimoorCV.pdf';
+    const a = document.createElement('a');
+    a.href = cvUrl;
+    a.download = 'Taimoor.pdf';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+
+  }
 }
