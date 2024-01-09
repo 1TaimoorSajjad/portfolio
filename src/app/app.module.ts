6b6a955354f5a8c2f756c84431457c1cd26e0481
1taimoorsajjad <taimoorsajjad980@gmail.com>
2024-01-10 00:02:42 +0500

    folder layout changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { FooterComponent } from './components/layouts/footer/footer.component';
+import { HeaderComponent } from './components/layouts/header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
